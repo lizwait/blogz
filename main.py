f2e1c682e7dc7504e820fbb1be9facbb2e883f8c
Liz Wait <elizabeth.m.wait@gmail.com>
2018-12-02 11:22:19 -0600

    Final Commit-- All done!

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class User(db.Model):
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.Text(120))
+    title = db.Column(db.String(120))
     body = db.Column(db.Text(300))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
@@ -50,7 +50,7 @@ def register():
             password_error2 = "Please enter valid password."
 
         if username_error != '' or password_error1 != '' or password_error2 != '':
-            return render_template('signup.html', username_error=username_error, password_error1=password_error1, password_error2=password_error2)
+            return render_template('signup.html', username_error=username_error, password_error1=password_error1, password_error2=password_error2, userName=user)
 
         if username_error == '' and password_error1 == '' and password_error2 == '':
             existing_user = User.query.filter_by(username=user).first()
@@ -61,7 +61,9 @@ def register():
                 session['user_id'] = new_user.id
                 session['User']= user
                 return redirect('/newpost')
-
+            else:
+                user_exists_error="Whoops! This username already exits. Please try again."
+                return render_template('signup.html', user_exists_error=user_exists_error, userName=user)
     return render_template('signup.html')
 
 @app.before_request
@@ -104,9 +106,8 @@ def post_login():
 
     else:
         verification_error= "Please enter a valid username and password combination."
-        return render_template('login.html', verification_error=verification_error)
+        return render_template('login.html', verification_error=verification_error, userName=user)
     
-
 @app.route('/blog', methods=['GET'])
 def blog_entries():
     blogs = Blog.query.all()
@@ -153,12 +154,12 @@ def new_blog_entry2():
         db.session.commit()
         return redirect('/blog?id=' + str(new_blog.id))
 
-    return render_template('newpost.html', titleError=titleError, bodyError=bodyError)
-
+    return render_template('newpost.html', titleError=titleError, bodyError=bodyError, blogBody=body, title=title)
 
 @app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
     return redirect('/login')
 
-app.run()
+if __name__=='__main__':
+    app.run()
